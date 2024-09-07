775a9578cef2b5d4b8a8d4feb60e1e74babb7638
Sammy Farida <samer.farida@yahoo.com>
2024-09-06 21:26:10 -0400

    updated personal configs

--- settings.js
+++ settings.js
@@ -6,12 +6,12 @@ export default
     "auth": "microsoft", // or "microsoft or offline"
     
     "profiles": [
-        "./profiles/llama.json",
+        "./profiles/TurboAI.json",
         // add more profiles here, check ./profiles/ for more
         // more than 1 profile will require you to /msg each bot indivually
     ],
-    "load_memory": false, // load memory from previous session
+    "load_memory": true, // load memory from previous session
     "init_message": "Say hello world and your name", // sends to all on spawn
-    "allow_insecure_coding": false, // disable at own risk
+    "allow_insecure_coding": true, // disable at own risk
     "code_timeout_mins": 10, // -1 for no timeout
 }
